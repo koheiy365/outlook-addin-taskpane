661732774e68f13111d516d0d1d5bcb7f3997359
koheiy365 <koheiy@tyisdev365.onmicrosoft.com>
2026-06-17 11:42:00 +0900

    Add files via upload

--- TaskPane.js
+++ TaskPane.js
@@ -286,7 +286,7 @@ GPOutlookExtension.OpenInfoCallLinkForOfficeAddin = function()
     var hash = location.hash;
     var hrefWithoutHash = hrefWitoutSerch.replace(hash, "");
     //var infoCallPagePath = hrefWithoutHash + "/../../../../OutlookAddinPicker.aspx";
-    var infoCallPagePath = "https://infosharedev.sharepoint.com/sites/Dev_KoheiY15/InfoshareProductLibrary/Sources/Infoshare.InfoCall/Extensions/Outlook/Sources/OutlookAddinPicker.aspx;
+    var infoCallPagePath = "https://infosharedev.sharepoint.com/sites/Dev_KoheiY15/InfoshareProductLibrary/Sources/Infoshare.InfoCall/Extensions/Outlook/Sources/OutlookAddinPicker.aspx";
 
     var queryString = "?"
                     + "Platform="
